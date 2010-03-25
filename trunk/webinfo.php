53002cbe4b1ee2201a4ed0f059a08cedd983fc39
lethargicprince <lethargicprince@5b250b3d-4c53-2ff1-2781-0226fc3f4c7b>
2010-03-25 08:04:46 +0000

    WebInfo.php

--- trunk/webinfo.php
+++ trunk/webinfo.php
@@ -3,8 +3,7 @@ require('./config/config.php');
 require('./classes/WebInfo.php');
 
 $wi = new WebInfo;
-$wi->get_imdb_source('tt0947810','title');
 
-print_r($wi->imdb_info);
-
+print_r($wi->get_imdb('com','tt0947810','title'));
+print_r($wi->get_imdb('de','tt0947810','title'));
 ?>
